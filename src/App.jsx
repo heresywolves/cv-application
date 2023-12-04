3d1c9056cac5979b12b491e8a6ef9a339e565bfb
Howl Kimura <howlkimuradev@gmail.com>
2023-12-04 20:16:47 +0300

    Separate education input to a component

--- src/App.jsx
+++ src/App.jsx
@@ -180,72 +180,13 @@ function App() {
             handleDeleteSkill={handleDeleteSkill}
             handleAddSkill={handleAddSkill}
           />
-          <div className="education">
-            <Spoiler
-              name="Education"
-              isOpen={false}
-              content={
-                <>
-                  {educationList.map(
-                    (item) => {
-                      return (
-                        <Spoiler
-                          key={item.id}
-                          name={(item.school === '') ? 'New School' : item.school}
-                          isOpen={false}
-                          content={
-                            <div key={item.id} className='education-entry-form'>
-                              <label htmlFor={'school-' + item.id}>School</label>
-                              <input 
-                                value={item.school} 
-                                onChange={handleEducationChange} 
-                                type="text" 
-                                id={'school-' + item.id}
-                              />
-                              <label htmlFor={'degree-' + item.id}>Degree</label>
-                              <input 
-                                value={item.degree} 
-                                onChange={handleEducationChange}
-                                type="text" 
-                                id={'degree-' + item.id}
-                              />
-                              <label htmlFor={'start-' + item.id}>Start Date</label>
-                              <input 
-                                value={item.start} 
-                                onChange={handleEducationChange}
-                                type="text" 
-                                id={'start-' + item.id}
-                              />
-                              <label htmlFor={'end-' + item.id}>End Date</label>
-                              <input 
-                                value={item.end} 
-                                onChange={handleEducationChange}
-                                type="text" 
-                                id={'end-' + item.id}
-                              />
-                              <label htmlFor={'location-' + item.id}>Location</label>
-                              <input 
-                                value={item.location} 
-                                onChange={handleEducationChange}
-                                type="text" 
-                                id={'location-' + item.id}
-                              />
-                              <button 
-                                className={"delete-entry " + item.id} 
-                                onClick={handleDeleteEducation}
-                              >Remove</button>
-                            </div>
-                          }
-                        />
-                      )
-                    }
-                    )
-                  }
-                  <button onClick={handleAddEducation}>+ Add</button>
-                </>
-              }
-            />
-          </div>
+          <EducationInput
+            educationList={educationList}
+            handleEducationChange={handleEducationChange}
+            handleAddEducation={handleAddEducation}
+            handleDeleteEducation={handleDeleteEducation}
+          />
+          
           <div className="experience">
             <Spoiler
               name="Experience"
@@ -371,6 +312,77 @@ function App() {
           )}
         </div>
       </div>
+    </div>
+  )
+}
+
+function EducationInput(props) {
+  return (
+    <div className="education">
+      <Spoiler
+        name="Education"
+        isOpen={false}
+        content={
+          <>
+            {props.educationList.map(
+              (item) => {
+                return (
+                  <Spoiler
+                    key={item.id}
+                    name={(item.school === '') ? 'New School' : item.school}
+                    isOpen={false}
+                    content={
+                      <div key={item.id} className='education-entry-form'>
+                        <label htmlFor={'school-' + item.id}>School</label>
+                        <input 
+                          value={item.school} 
+                          onChange={props.handleEducationChange} 
+                          type="text" 
+                          id={'school-' + item.id}
+                        />
+                        <label htmlFor={'degree-' + item.id}>Degree</label>
+                        <input 
+                          value={item.degree} 
+                          onChange={props.handleEducationChange}
+                          type="text" 
+                          id={'degree-' + item.id}
+                        />
+                        <label htmlFor={'start-' + item.id}>Start Date</label>
+                        <input 
+                          value={item.start} 
+                          onChange={props.handleEducationChange}
+                          type="text" 
+                          id={'start-' + item.id}
+                        />
+                        <label htmlFor={'end-' + item.id}>End Date</label>
+                        <input 
+                          value={item.end} 
+                          onChange={props.handleEducationChange}
+                          type="text" 
+                          id={'end-' + item.id}
+                        />
+                        <label htmlFor={'location-' + item.id}>Location</label>
+                        <input 
+                          value={item.location} 
+                          onChange={props.handleEducationChange}
+                          type="text" 
+                          id={'location-' + item.id}
+                        />
+                        <button 
+                          className={"delete-entry " + item.id} 
+                          onClick={props.handleDeleteEducation}
+                        >Remove</button>
+                      </div>
+                    }
+                  />
+                )
+              }
+              )
+              }
+              <button onClick={props.handleAddEducation}>+ Add</button>
+            </>
+          }
+          />
     </div>
   )
 }
