4e6ae74052b7b36e8028a0593f3a11d6eafc6525
Howl Kimura <howlkimuradev@gmail.com>
2023-12-04 11:06:59 +0300

    Delete everything but layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PropTypes from 'prop-types';
+import Spoiler from './components/Spoiler'
 
 
 function App() {
   // const [count, setCount] = useState(0)
-  let mode = 'content';
+  function handeSendForm(e) {
+    e.preventDefault();
+  }
   
   return (
     <div className="content-container">
       <div className="form-container">
-        <OptionModeButtons/>
-        <Form mode={mode}/>
+        <form onSubmit={handeSendForm}>
+          <label htmlFor="nameInput">Full name</label>
+          <input type="text" id="nameInput"/>
+          <label htmlFor="emailInput">Email</label>
+          <input type="text" id="emailInput"/>
+          <label htmlFor="phoneInput">Phone number</label>
+          <input type="text" id="phoneInput"/>
+          <label htmlFor="addressInput">Address</label>
+          <input type="text" id="addressInput"/>
+        </form>
       </div>
       <div className="preview-container">
       </div>
@@ -21,97 +31,6 @@ function App() {
   )
 }
 
-const Spoiler = ({ name, content, isOpen }) => {
-  const [isSpoilerVisible, setSpoilerVisible] = useState(isOpen);
-  const toggleSpoiler = () => {
-    setSpoilerVisible(!isSpoilerVisible);
-  }
 
-  return (
-    <div>
-      <button onClick={toggleSpoiler}>
-        {name}
-      </button>
-      {isSpoilerVisible && content}
-    </div>
-  )
-}
-
-function OptionModeButtons() {
-  return (
-    <div className='option-mode-button-container'>
-      <button>Content</button>
-      <button>Layout</button>
-    </div>
-  )
-}
-
-function Form({mode}) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
-
-  if (mode === 'content') {
-    return (
-      <form onSubmit={handleSubmit}>
-        <Spoiler
-          name="Personal Information"
-          content={
-            <div className='personal-info-form-container'>
-              <label htmlFor="nameField">Full name</label>
-              <input type="text" id="nameField"/>
-              <label htmlFor="emailField">Email</label>
-              <input type="text" id="emailField"/>
-              <label htmlFor="phoneField">Phone number</label>
-              <input type="text" id="phoneField"/>
-              <label htmlFor="addressField">Address</label>
-              <input type="text" id="addressField"/>
-            </div>
-          }
-          isOpen={true}
-        />
-        <Spoiler
-          name="Education"
-          content={<EducationList/>}
-          isOpen={false}
-        />
-      </form>
-    )
-  }
-
-  return null; // Return null if the mode is not 'content' or 'layout'
-}
-
-
-const EducationList = () => {
-  const [educationEntries, setEducationEntries] = useState([
-    { school: 'Harvard', degree: 'Masters', start: '1994', end: '2001'},
-    { school: 'High School', degree: 'GED', start: '1981', end: '1994'},
-  ])
-
-  const handleChange = (index, event) => {
-
-  }
-
-  const renderEducationList = () => {
-    return educationEntries.map((entry, index) => ( 
-      <div key={"education-entry-" + index}>
-        <p>{entry.school}</p>
-      </div>
-    ));
-  }
-
-  return (
-    <div>
-      {renderEducationList()}
-    </div>
-  )
-}
-
-// Insures that the 'mode' prop is a required string. Will throw
-// A warning if the prop is missing or has wrong type
-Form.propTypes = {
-  mode: PropTypes.string.isRequired,
-}
 
 export default App
---
+++ src/components/Spoiler.jsx
@@ -0,0 +1,17 @@
+const Spoiler = ({ name, content, isOpen }) => {
+  const [isSpoilerVisible, setSpoilerVisible] = useState(isOpen);
+  const toggleSpoiler = () => {
+    setSpoilerVisible(!isSpoilerVisible);
+  }
+
+  return (
+    <div>
+      <button onClick={toggleSpoiler}>
+        {name}
+      </button>
+      {isSpoilerVisible && content}
+    </div>
+  )
+}
+
+export default Spoiler;
